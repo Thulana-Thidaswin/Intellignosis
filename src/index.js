682b19a51665f3b9d9395b33eb3cc6c359e3e31b
harees-srini <74543829+harees-srini@users.noreply.github.com>
2022-02-09 23:50:52 +0530

    Renamed properly

--- src/index.js
+++ src/index.js
@@ -94,7 +94,7 @@ const createWindow = () => {
   const menu = Menu.buildFromTemplate(template)
   Menu.setApplicationMenu(menu)
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'SignUpScreen.html'));
+  mainWindow.loadFile(path.join(__dirname, 'SignUpScreenHTML.html'));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();

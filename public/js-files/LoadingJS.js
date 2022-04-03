e8cf1e21ceb5d5120e0829af9246baf471ae9434
ArifZahir <arifzahir77@gmail.com>
2022-04-03 14:09:23 +0300

    replaced files

---
+++ public/js-files/LoadingJS.js
@@ -0,0 +1,1 @@
+setTimeout(() => {  window.location.href="DashboardScreenHTML.html"; }, 3750);
--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,73 @@ router.get('/dashboard', (req, res) => {
     res.render('dashboard');
 })
 
+router.get('/AboutHTML', (req, res) => {
+    res.render('AboutHTML');
+})
+
+router.get('/AccountPreview', (req, res) => {
+    res.render('AccountPreview');
+})
+
+router.get('/AccountSettingsHTML', (req, res) => {
+    res.render('AccountSettingsHTML');
+})
+
+router.get('/AddressChangeHTML', (req, res) => {
+    res.render('AddressChangeHTML');
+})
+
+router.get('/AnalysisLoadingHTML', (req, res) => {
+    res.render('AnalysisLoadingHTML');
+})
+
+router.get('/AnalyzeScreenHTML', (req, res) => {
+    res.render('AnalyzeScreenHTML');
+})
+
+
+router.get('/BirthdatechangeHTML', (req, res) => {
+    res.render('BirthdatechangeHTML');
+})
+
+
+router.get('/ChangeScreenHTML', (req, res) => {
+    res.render('ChangeScreenHTML');
+})
+
+router.get('/ConsultantScreenHTML', (req, res) => {
+    res.render('ConsultantScreenHTML');
+})
+
+router.get('/EmailChangeHTML', (req, res) => {
+    res.render('EmailChangeHTML');
+})
+
+router.get('/MoreInHTML', (req, res) => {
+    res.render('MoreInHTML');
+})
+
+
+router.get('/NamechangeHTML', (req, res) => {
+    res.render('NamechangeHTML');
+})
+
+router.get('/NewPatientHTML', (req, res) => {
+    res.render('NewPatientHTML');
+})
+
+router.get('/OccupationChangeHTML', (req, res) => {
+    res.render('OccupationChangeHTML');
+})
+
+router.get('/ResultsScreenHTML', (req, res) => {
+    res.render('ResultsScreenHTML');
+})
+
+router.get('/SettingsScreenHTML', (req, res) => {
+    res.render('SettingsScreenHTML');
+})
+
+
+
 module.exports = router;

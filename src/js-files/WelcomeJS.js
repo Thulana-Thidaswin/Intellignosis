7b14e23ff5fb0ec69468392439d8e3984b7c9d23
ArifZahir <arifzahir77@gmail.com>
2022-04-18 01:42:14 +0300

    Styled Welcome Page

--- src/js-files/WelcomeJS.js
+++ src/js-files/WelcomeJS.js
@@ -1,19 +1,19 @@
 
 window.addEventListener("load", (event) => {
     const statusDisplay = document.getElementById("status");
-    statusDisplay.textContent = navigator.onLine ? "Status: Online" : "Status: Offline";
+    statusDisplay.textContent = navigator.onLine ? ">>> Status: Online " : ">>> Status: Offline ";
 });
     
 window.addEventListener("offline", async (event) => {
     const statusDisplay = document.getElementById("status");
-    statusDisplay.textContent = "Status: Offline";
+    statusDisplay.textContent = ">>> Status: Offline ";
     document.getElementById("maiden").style.display = "none";
     
   });
   
   window.addEventListener("online", async (event) => {
     const statusDisplay = document.getElementById("status");
-    statusDisplay.textContent = "Status: Online";
+    statusDisplay.textContent = ">>> Status: Online ";
     document.getElementById("maiden").style.display = "block";
     
   });

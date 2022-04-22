fdd6237fa43f27add534614f3baf10595cf62aaf
harees-srini <74543829+harees-srini@users.noreply.github.com>
2022-04-22 13:02:17 +0530

    Displaying Date and output

--- app.py
+++ app.py
@@ -97,4 +97,4 @@ def index():
     return predAsString
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, use_reloader=False)
+    app.run(debug=True, port=5000, use_reloader=False)  
